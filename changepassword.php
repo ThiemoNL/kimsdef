231927d33beb63f835ef9cfb583f7b3dd0ca79d6
ThiemoNL <tim_oskam@outlook.com>
2019-06-16 23:04:29 +0200

    Change password toegevoegd

--- changepassword.php
+++ changepassword.php
@@ -1,7 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tim_O
- * Date: 17-5-2019
- * Time: 08:46
- */
+require_once 'core/init.php';
+
+$user = new User();
+if(!$user->isLoggedIn()){
+    Redirect::to('index.php');
+}
+
+if(Input::exists()){
+    if(Token::check(Input::get('token'))){
+
+        $validate = new Validate();
+        $validation = $validate->check($_POST,[
+            'password_current' => [
+                'required' => true,
+                'min' => 6
+             ],
+            'password_new' => [
+                'required' => true,
+                'min' => 6,
+            ],
+            'password_new_again' => [
+                'required' => true,
+                'min' => 6,
+                'matches' => 'password_new'
+            ]
+
+        ]);
+
+        if($validation->passed()){
+
+
+
+            if(!password_verify(Input::get('password_current'), $user->data()->password)){
+                echo 'Your curerent password is wrong';
+            } else {
+                $user->update([
+                    'password' => Hash::make(Input::get('password_new'))
+                ]);
+
+                Session::flash('home', 'Your password have been changed');
+                Redirect::to('index.php');
+            }
+        } else {
+            foreach ($validation->errors() as $error){
+                echo $error, '<br>';
+            }
+        }
+
+    }
+}
+?>
+
+
+
+<form action="" method="post">
+    <div class="field">
+        <label for="password_current">Currenct password</label>
+        <input type="password" name="password_current" id="password_current">
+    </div>
+
+    <div class="field">
+        <label for="password_new">New Password</label>
+        <input type="password" name="password_new" id="password_new">
+    </div>
+
+    <div class="field">
+        <label for="password_new_again">New Password again</label>
+        <input type="password" name="password_new_again" id="password_new_again">
+    </div>
+
+    <input type="submit" value="Change">
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+</form>
--- index.php
+++ index.php
@@ -5,31 +5,17 @@ if(Session::exists('home')) {
     echo '<p>' . Session::flash('home') . '</p>';
 }
 
-//$user = DB::getInstance()->get('users', ['username', '=', 'alex']);
-//$userInsert = DB::getInstance()->update('users', 1, [
-//    'username' => 'test',
-//    'password' => 'newwpassword',
-//    'salt' => 'mewsalt'
-//    ]);
-//
-//if(!$userInsert) {
-//    echo "niet gelukt";} else{
-//
-//    echo "het is gelukt";
-//}
-
-
-echo '<br>';
 $user = new User();
 
 if($user->isLoggedIn()){
     ?>
 
-    <p>Hello <a href="#"><?= htmlentities($user->data()->username) ?></a>!</p>
+    <p>Hello <a href="#"><?= escape($user->data()->name) ?></a>!</p>
 
     <ul>
         <li><a href="logout.php">Log Out</a></li>
         <li><a href="update.php">Update details</a></li>
+        <li><a href="changepassword.php">Change Password</a></li>
     </ul>
 
 <?php
